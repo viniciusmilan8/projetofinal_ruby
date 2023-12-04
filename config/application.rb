f2f35afa1c1b4281492dbe20f1cd8ec5b8d69e1c
Vinicius <viniciusmilan8@gmail.com>
2023-12-03 23:45:57 -0300

    Erro ao tentar instalar bootstrap

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Projetofinal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Brasilia'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
